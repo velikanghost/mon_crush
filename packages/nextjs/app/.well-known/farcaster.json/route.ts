7746ae797b79a6cdc6eced504d2e8f43625fc11b
velikanghost <velikanghost@yahoo.com>
2025-05-13 11:31:02 +0100

    updating manifest file

--- packages/nextjs/app/.well-known/farcaster.json/route.ts
+++ packages/nextjs/app/.well-known/farcaster.json/route.ts
@@ -6,19 +6,19 @@ export async function GET() {
     accountAssociation: {
       header:
         "eyJmaWQiOjUxODMzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDI0QjcyODlhZTRkN0FEZTY3QjdmMzgzNDFFYjc4Yzc3NjAyODIxOTUifQ",
-      payload: "eyJkb21haW4iOiJtb24tY3J1c2gtbmV4dGpzLnZlcmNlbC5hcHAifQ",
+      payload: "eyJkb21haW4iOiJtb24tY3J1c2gtbmV4dGpzLWZpdmUudmVyY2VsLmFwcCJ9",
       signature:
-        "MHhiZTU3YTdhY2U5YjI2ODkyYmE0MjBkMzU4NzgzMDQ0Njc5MzIwNTNjMTU2MDkzNTcxNTVhNTA3YTlkOTg5YjIwMTJhY2U1NDFjYTg0Njg0Y2EyODYwZDNhMjUxMTcyOTFkNDc4NDJmYzU5YjVkYmFiNTkwOWJhNWQ3MmZiYWIzNjFj",
+        "MHgxNDA4MjUxODMzMzQxYTY4ZWNmZmUzMTA2ZWQ3MGQ2ZDVlNmI2N2Y1YWMwYjcxNDU4NGY5YzU5MGNiY2Y4NjNjNDg2Zjg0Njk5NjJmY2MzYjVkNTQxYTM3NjEwZTlkM2MwYzg2ZGE5MzAzYjJjZGZjNWZhYjkyYTM5Yzk0YTRjNjFj",
     },
     frame: {
-      version: "1",
+      version: "1.1",
       name: "Monad Match",
       iconUrl: `${APP_URL}/images/icon.png`,
       homeUrl: `${APP_URL}`,
       imageUrl: `${APP_URL}/images/feed.png`,
       screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp"],
-      primaryCategory: "developer-tools",
+      tags: ["monad", "farcaster", "miniapp", "games"],
+      primaryCategory: "games",
       buttonTitle: "Launch App",
       splashImageUrl: `${APP_URL}/images/splash.png`,
       splashBackgroundColor: "#ffffff",
