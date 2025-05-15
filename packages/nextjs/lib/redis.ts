cf54917a6f7f7e31203b542aefeb8945bca168f2
velikanghost <velikanghost@yahoo.com>
2025-05-15 06:26:30 +0100

    upstash url

--- packages/nextjs/lib/redis.ts
+++ packages/nextjs/lib/redis.ts
@@ -1,16 +1,16 @@
 import { Redis } from "@upstash/redis";
 import { env } from "process";
 
-if (!env.MON_REDIS_URL || !env.MON_KV_REST_API_TOKEN) {
+if (!env.MON_KV_REST_API_URL || !env.MON_KV_REST_API_TOKEN) {
   console.warn(
     "REDIS_URL or REDIS_TOKEN environment variable is not defined, please add to enable background notifications and webhooks.",
   );
 }
 
 export const redis =
-  env.MON_REDIS_URL && env.MON_KV_REST_API_TOKEN
+  env.MON_KV_REST_API_URL && env.MON_KV_REST_API_TOKEN
     ? new Redis({
-        url: env.MON_REDIS_URL,
+        url: env.MON_KV_REST_API_URL,
         token: env.MON_KV_REST_API_TOKEN,
       })
     : null;
